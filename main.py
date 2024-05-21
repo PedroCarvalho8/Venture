b8f6d1859dc9870c2b1a1b94072d4f33d9c013f8
PedroCarvalho8 <pedrohpcarvalho@outlook.com>
2024-05-21 12:37:48 -0300

    added the project to the repo

---
+++ main.py
@@ -0,0 +1,44 @@
+import cv2
+import streamlit as st
+from ultralytics import YOLO
+
+# Load model
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+
+col1, col2 = st.columns(2)
+
+if col1.button("Iniciar"):
+    if col2.button("Parar"):
+        st.rerun()
+
+    with st.empty():
+        while True:
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results = model(frame)
+
+            for result in results:
+                boxes = result.boxes.cpu().numpy()
+                xyxys = boxes.xyxy
+                
+                names = result.names
+                cls = int(result.boxes.cls[0])
+
+                for xyxy in xyxys:
+                    
+                    cv2.putText(frame, names[cls], (int(xyxy[0]), int(xyxy[1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                    cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (0, 255, 0), 3)
+
+
+            st.image(frame)
+           
+    
+
